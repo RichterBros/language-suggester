c1e6aab8aaca8dd8ec1e3c2b98d99546a1e73e0f
Erich Richter <erich.richter@gmail.com>
2020-04-30 02:02:45 -0700

    add html and js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,7 @@
 $(document).ready(function(){
-       
-   
-      
-$("#beverage").change(function() {
-      
-    
-    
+    $( "#results" ).click(function() {
     option.selections();
-});
-
-$("#movie").change(function() {
-
-     
-
-option.selections();
-
-
-});
-            
-            
-    
-
-
-
-
-
+  });  
 });
 
 
@@ -33,11 +10,12 @@ var option = new Options()
 
 
 
-function Options(option1,option2,option3,option4){
+function Options(option1,option2,option3,option4, option5){
      this.option1 = option1;
      this.option2 = option2;
      this.option3 = option3;
      this.option4 = option4;
+     this.option5 = option5;
     }
     
     
@@ -47,10 +25,12 @@ function Options(option1,option2,option3,option4){
 
     Options.prototype.selections= function(){
     this.option1 = parseInt($("#beverage").val())
-    this.option2 = parseInt($("movie").val())
-        
+    this.option2 = parseInt($("#movie").val())
+    this.option3 = parseInt($("#animal").val())
+    this.option4 = parseInt($("#color").val())
+    this.option5 = parseInt($("#music").val())     
     
-    if (this.option1 === 1){
+    if (this.option1 === 1 && this.option2 === 1 && this.option3 === 1 && this.option4 === 1 && this.option5 ===1){
     console.log("hi")
 }
 
