bebd863a2d3d09b1128ba551dc72f3ede794aab2
Erich Richter <erich.richter@gmail.com>
2020-04-30 03:02:14 -0700

    complete functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,35 @@
 $(document).ready(function(){
-    $( "#results" ).click(function() {
+    $("#submit").click(function() {
     option.selections();
   });  
 });
 
-
-var option = new Options()
-
-
-
-
 function Options(option1,option2,option3,option4, option5){
-     this.option1 = option1;
-     this.option2 = option2;
-     this.option3 = option3;
-     this.option4 = option4;
-     this.option5 = option5;
-    }
-    
-    
-
-    
-     
-
-    Options.prototype.selections= function(){
-    this.option1 = parseInt($("#beverage").val())
-    this.option2 = parseInt($("#movie").val())
-    this.option3 = parseInt($("#animal").val())
-    this.option4 = parseInt($("#color").val())
-    this.option5 = parseInt($("#music").val())     
-    
-    if (this.option1 === 1 && this.option2 === 1 && this.option3 === 1 && this.option4 === 1 && this.option5 ===1){
-    console.log("hi")
+  this.option1 = option1;
+  this.option2 = option2;
+  this.option3 = option3;
+  this.option4 = option4;
+  this.option5 = option5;
 }
-
-
-    }
-
-
-
-
-
-
-
-
+    
+var option = new Options()
+    
+Options.prototype.selections= function(){
+  this.option1 = parseInt($("#beverage").val())
+  this.option2 = parseInt($("#movie").val())
+  this.option3 = parseInt($("#animal").val())
+  this.option4 = parseInt($("#color").val())
+  this.option5 = parseInt($("#music").val())     
+    
+if (this.option1 + this.option2 + this.option3 + this.option4 + this.option5 <= 5){
+  $("#results").text("your language is C#")
+  }else if (this.option1 + this.option2 + this.option3 + this.option4 + this.option5 > 5 &&  this.option1 + this.option2 + this.option3 + this.option4 + this.option5 <= 10){
+    $("#results").text("your language is JavaScript")
+  }else if (this.option1 + this.option2 + this.option3 + this.option4 + this.option5 > 10 &&  this.option1 + this.option2 + this.option3 + this.option4 + this.option5 <= 15){
+    $("#results").text("your language is Python")
+  }else if (this.option1 + this.option2 + this.option3 + this.option4 + this.option5 > 15 &&  this.option1 + this.option2 + this.option3 + this.option4 + this.option5 <= 20){
+    $("#results").text("your language is Ruby")
+  }else{
+    $("#results").text("please choose one of each option")
+  }
+}
